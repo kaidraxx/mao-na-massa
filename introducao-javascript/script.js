9d351dcc37fccd6b1fc01198bac7ecfdcb39a49d
kaidraxx <163013393+kaidraxx@users.noreply.github.com>
2024-09-17 08:28:14 -0300

    alterei o imc

--- introducao-javascript/script.js
+++ introducao-javascript/script.js
@@ -1,2 +1,10 @@
 var titulo = document.querySelector('h1');
 titulo.textContent = "Aparecida Nutricionista";
+
+var paulo = document.querySelector('#paciente1');
+var peso = paulo.querySelector('.info-peso').textContent;
+var altura = paulo.querySelector('.info-altura').textContent;
+
+var imc = peso / (altura * altura);
+
+paulo.querySelector('.info-imc').textContent = imc;
