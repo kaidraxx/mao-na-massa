1e8568f1d4b7159b39468594ffb893a03b0606ba
kaidraxx <163013393+kaidraxx@users.noreply.github.com>
2024-09-23 12:05:18 -0300

    modificar todos os imc de um vez

--- introducao-javascript/script.js
+++ introducao-javascript/script.js
@@ -1,10 +1,21 @@
 var titulo = document.querySelector('h1');
 titulo.textContent = "Aparecida Nutricionista";
 
-var paulo = document.querySelector('#paciente1');
-var peso = paulo.querySelector('.info-peso').textContent;
-var altura = paulo.querySelector('.info-altura').textContent;
+// var paulo = document.querySelector('#paciente1');
+// var peso = paulo.querySelector('.info-peso').textContent;
+// var altura = paulo.querySelector('.info-altura').textContent;
 
-var imc = peso / (altura * altura);
+// var imc = peso / (altura * altura);
 
-paulo.querySelector('.info-imc').textContent = imc;
+// paulo.querySelector('.info-imc').textContent = imc;
+
+var pacientes = document.querySelectorAll('.paciente');
+for(let contador = 0; contador < pacientes.length; contador++){
+    var peso = pacientes[contador].querySelector('.info-peso').textContent;
+    var altura = pacientes[contador].querySelector('.info-altura').textContent;
+    var imc = peso / (altura * altura);
+
+    imc = parseFloat(imc);
+
+    pacientes[contador].querySelector('.info-imc').textContent = imc.toFixed(2);
+}
